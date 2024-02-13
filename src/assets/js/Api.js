227aa8ff630348318c32ad652f50b813877fa3b5
Syed Abdul Rehman <syedabdulrehman05@gmail.com>
2024-02-13 21:51:28 +0500

    update

--- src/assets/js/Api.js
+++ src/assets/js/Api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://127.0.0.1:3000/api",
+  // baseURL: "http://127.0.0.1:3000/api",
+  baseURL: "https://admin-api.blinkersmarketing.com/api"
 });
 
 export default Api;
