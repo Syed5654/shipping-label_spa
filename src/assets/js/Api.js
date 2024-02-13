13ab05a107159a4086a6150644ad3422c314a331
Syed Abdul Rehman <syedabdulrehman05@gmail.com>
2024-02-13 23:44:35 +0500

    updated api

--- src/assets/js/Api.js
+++ src/assets/js/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: "http://127.0.0.1:3000/api",
-  baseURL: "https://admin-api.blinkersmarketing.com/api"
+  baseURL: "https://admin.ecomfulfil.store/api"
 });
 
 export default Api;
