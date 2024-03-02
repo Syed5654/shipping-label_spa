9bb4050a3073e5fb783647d8ccb2a0206f6c4e48
Syed Abdul Rehman <syedabdulrehman05@gmail.com>
2024-03-02 11:35:44 +0500

    added email in footer

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="footer">
     <div class="bg-secondary py-2 text-center">
+      <a href="mailto:support@ecomfulfil.store" class="mb-0 text-white font-medium text-decoration-none ">Contact: support@ecomfulfil.store</a>
       <p class="mb-0 text-white font-medium">&copy; All rights reserved</p>
     </div>
   </div>
